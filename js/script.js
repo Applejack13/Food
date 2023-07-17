40a57957f166193d46da9b3c6edbc7eee61b9b1e
Vadym <andreeshinvadim13@gmail.com>
2023-07-17 22:11:44 +0300

    added some npm and npx
    Babel+Core.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-import calc from "./modules/calc";
-import cards from "./modules/cards";
-import forms from "./modules/forms";
-import modal from "./modules/modal";
-import slider from "./modules/slider";
-import tabs from "./modules/tabs";
-import timer from "./modules/timer";
-import { openModal } from "./modules/modal";
+require("es6-promise").polyfill()
+import "nodelist-foreach-polyfill"
+
+import calc from "./modules/calc"
+import cards from "./modules/cards"
+import forms from "./modules/forms"
+import modal from "./modules/modal"
+import slider from "./modules/slider"
+import tabs from "./modules/tabs"
+import timer from "./modules/timer"
+import { openModal } from "./modules/modal"
 
 window.addEventListener("DOMContentLoaded", function () {
   const modalTimerId = setTimeout(
     () => openModal(".modal", modalTimerId),
     300000
-  );
+  )
 
-  calc();
-  cards();
-  forms("form", modalTimerId);
-  modal("[data-modal]", ".modal", modalTimerId);
+  calc()
+  cards()
+  forms("form", modalTimerId)
+  modal("[data-modal]", ".modal", modalTimerId)
   slider({
     container: ".offer__slider",
     slide: ".offer__slide",
@@ -26,12 +29,12 @@ window.addEventListener("DOMContentLoaded", function () {
     currentCounter: "#current",
     wrapper: ".offer__slider-wrapper",
     field: ".offer__slider-inner",
-  });
+  })
   tabs(
     ".tabheader__item",
     ".tabcontent",
     ".tabheader__items",
     "tabheader__item_active"
-  );
-  timer(".timer", "2023-09-10 9:00:00");
-});
+  )
+  timer(".timer", "2023-09-10 9:00:00")
+})
